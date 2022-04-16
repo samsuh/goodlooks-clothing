77dd34016fe5c02f151cb39a63cfd9553cbfbdfa
samuel suh <samsuh@gmail.com>
2022-04-16 19:28:25 +0900

    products card component added to shop page

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,5 +1,17 @@
+import { useContext } from 'react'
+import { ProductsContext } from '../../contexts/products.context'
+import ProductCard from '../../components/product-card/product-card.component'
+import './shop.styles.scss'
+
 const Shop = () => {
-  return <div>Shop Component</div>
+  const { products } = useContext(ProductsContext)
+  return (
+    <div className='products-container'>
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product}></ProductCard>
+      ))}
+    </div>
+  )
 }
 
 export default Shop
