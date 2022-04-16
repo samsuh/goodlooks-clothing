52d317b18b4315af326e8489dba5fd16a004c9eb
samuel suh <samsuh@gmail.com>
2022-04-16 20:49:13 +0900

    add shopping cart and cartContext

--- src/routes/navbar/navbar.component.jsx
+++ src/routes/navbar/navbar.component.jsx
@@ -1,12 +1,16 @@
 import { useContext } from 'react'
 import { Outlet, Link } from 'react-router-dom'
+import CartIcon from '../../components/cart-icon/cart-icon.component'
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component'
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
 import './navigation.styles.scss'
 import { UserContext } from '../../contexts/user.context'
+import { CartContext } from '../../contexts/cart.context'
 import { signOutUser } from '../../utils/firebase/firebase.utils'
 
 const NavigationBar = () => {
   const { currentUser } = useContext(UserContext)
+  const { isCartOpen } = useContext(CartContext)
   return (
     <>
       <div className='navigation'>
@@ -26,7 +30,9 @@ const NavigationBar = () => {
               SIGN IN
             </Link>
           )}
+          <CartIcon />
         </div>
+        {isCartOpen && <CartDropdown />}
       </div>
       <Outlet />
     </>
