645c1e4c396e9198f1e5f286f0bbb0d6cdf99b73
samuel suh <samsuh@gmail.com>
2022-04-18 17:32:59 +0900

    checkout page started, implemented increment and decrement cartItems

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './routes/home/home.component'
 import Shop from './routes/shop/shop.component'
 import NavigationBar from './routes/navbar/navbar.component.jsx'
 import Authentication from './routes/authentication/authentication.component'
+import CheckOut from './routes/checkout/checkout.component'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
         <Route path='auth' element={<Authentication />} />
+        <Route path='checkout' element={<CheckOut />} />
       </Route>
     </Routes>
   )
